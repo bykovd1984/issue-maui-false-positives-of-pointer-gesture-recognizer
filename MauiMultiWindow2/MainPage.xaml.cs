f78ab4d7ecc4c03d63cf5106f733145d6ac9bd7a
Denis Bykov <bykov@metaquotes.net>
2025-01-29 09:04:10 +0200

    initial

---
+++ MauiMultiWindow2/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿namespace MauiMultiWindow;
+
+public partial class MainPage : ContentPage
+{
+  int count = 0;
+
+  public MainPage()
+  {
+    InitializeComponent();
+  }
+
+  private void OnClicked(object sender, EventArgs e)
+  {
+    Window secondWindow = new Window(new SecondPage());
+    secondWindow.Width = 300;
+    secondWindow.Height = 300;
+    Application.Current?.OpenWindow(secondWindow);
+  }
+
+  private void OnPointerEntered(object sender, PointerEventArgs e)
+  {
+    grid.BackgroundColor = Colors.Red;
+  }
+
+  private void OnPointerExited(object sender, PointerEventArgs e)
+  {
+    grid.BackgroundColor = Colors.White;
+  }
+}
